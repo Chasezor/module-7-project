38d10dc465e625dd9551f78f99796b8a19ae8e35
Chasezor <chase.c.contact@gmail.com>
2021-10-29 12:17:54 -0600

    saving

--- pangramSentence.js
+++ pangramSentence.js
@@ -1,6 +1,6 @@
 const isPangram = (inputStr) =>{
     compStr = ("The quick brown fox jumps over the lazy dog!")
-
+    
     for(i = 0; i < inputStr.length; i++)
         if(inputStr[i] != compStr[i]){
             return true
--- uniqueCharacters.js
+++ uniqueCharacters.js
@@ -1,5 +1,22 @@
-hasUniqueChars("Monday");
+const hasUniqueChars = (str) =>{
+    let charTable = {};
+    for(i = 0; i < str.length; i++)
+    {
+    if(charTable[str[i]] !=null)
+    {
+        charTable[str[1]] = 1;
+        return false
+     }
+    else{
+        charTable[str[i]] = 0
+        }
+    }
+    return true
+}
+
+
+console.log(hasUniqueChars("Monday"));
 // -> True
 
-hasUniqueChars("Moonday");
+console.log(hasUniqueChars("Moonday"));
 // -> False
